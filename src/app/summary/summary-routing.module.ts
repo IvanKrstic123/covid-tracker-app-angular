4912c444311fd1f8f8604ebe6d95847ae66bd0f0
Ivan Krstic <krstic8687@yahoo.com>
2022-12-19 23:39:40 +0100

    created routes

--- src/app/summary/summary-routing.module.ts
+++ src/app/summary/summary-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SumaryPageComponent } from './components/sumary-page/sumary-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: SumaryPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
