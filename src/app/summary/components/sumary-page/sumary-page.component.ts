708cf74bc5c785174e3362c83858535c14d32b02
Ivan Krstic <krstic8687@yahoo.com>
2022-12-19 23:27:44 +0100

    created pages and layout

---
+++ src/app/summary/components/sumary-page/sumary-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sumary-page',
+  templateUrl: './sumary-page.component.html',
+  styleUrls: ['./sumary-page.component.css']
+})
+export class SumaryPageComponent {
+
+}
