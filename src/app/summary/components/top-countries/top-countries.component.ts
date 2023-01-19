e992310226501444d53ae7a585cae085f10ee61b
Ivan Krstic <krstic8687@yahoo.com>
2023-01-20 00:41:15 +0100

    finishing top countries on summary page

--- src/app/summary/components/top-countries/top-countries.component.ts
+++ src/app/summary/components/top-countries/top-countries.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-top-countries',
   templateUrl: './top-countries.component.html',
   styleUrls: ['./top-countries.component.css']
 })
-export class TopCountriesComponent {
+export class TopCountriesComponent implements OnInit, OnChanges {
 
+  @Input() covidData: any;
+  topConfirmedCases: any[] = [];
+  topConfirmedDeaths: any[] = [];
+  topConfirmedRecovered: any[] = [];
+
+  ngOnInit(): void {
+
+  }
+
+  ngOnChanges(): void {
+    // this.topConfirmedCases = this.covidData.Countries
+    // ?.sort((a: any, b: any) => b.TotalConfirmed - a.TotalConfirmed)
+    // .slice(0, 5);
+
+      this.topConfirmedCases = this.covidData.Countries
+      ?.sort((a: any, b: any) => b.TotalConfirmed - a.TotalConfirmed)
+      .slice(0, 5);
+
+      this.topConfirmedDeaths = this.covidData.Countries
+      ?.sort((a: any, b: any) => b.TotalDeaths - a.TotalDeaths)
+      .slice(0, 5);
+
+      this.topConfirmedRecovered = this.covidData.Countries
+      ?.sort((a: any, b: any) => b.TotalRecovered - a.TotalRecovered)
+      .slice(0, 5);
+  }
 }
