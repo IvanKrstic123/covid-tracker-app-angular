c3355b65efdfeac173882743f9720bc71d1bb091
Ivan Krstic <krstic8687@yahoo.com>
2023-01-19 23:52:15 +0100

    created markup for top countries

--- src/app/summary/summary.module.ts
+++ src/app/summary/summary.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { SummaryRoutingModule } from './summary-routing.module';
 import { SumaryPageComponent } from './components/sumary-page/sumary-page.component';
 import { SummaryCardsComponent } from './components/summary-cards/summary-cards.component';
+import { TopCountriesComponent } from './components/top-countries/top-countries.component';
 
 
 @NgModule({
   declarations: [
     SumaryPageComponent,
-    SummaryCardsComponent
+    SummaryCardsComponent,
+    TopCountriesComponent
   ],
   imports: [
     CommonModule,
