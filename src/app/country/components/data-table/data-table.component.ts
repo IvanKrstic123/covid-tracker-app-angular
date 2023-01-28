40f54cdf9feac1878b2a6c3de40e163d0a645f1f
Ivan Krstic <krstic8687@yahoo.com>
2023-01-28 21:18:19 +0100

    created markup for list of countries

---
+++ src/app/country/components/data-table/data-table.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-table',
+  templateUrl: './data-table.component.html',
+  styleUrls: ['./data-table.component.css']
+})
+export class DataTableComponent implements OnInit, OnChanges {
+
+  @Input() covidData: any;
+  countries: any[] = [];
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+  }
+
+  ngOnChanges(): void {
+    this.countries = this.covidData?.Countries;
+    console.log(this.countries);
+  }
+
+}
